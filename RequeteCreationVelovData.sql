249fef439d1789d428e78edec0a82f7a2a92a1a0
PierreRichaud <richaud.pierre@outlook.fr>
2016-05-25 19:34:28 +0200

    Useful SQL request

---
+++ RequeteCreationVelovData.sql
@@ -0,0 +1,3 @@
+﻿drop table velovData;
+create table velovData (station_name INTEGER, available_bikes INTEGER, last_update TIMESTAMP, last_update_fme TIMESTAMP, status varchar(20), lat float, lng float, bike_stands integer, available_bike_stands integer)
+create table stationLabel (station_name INTEGER, name VARCHAR, number INTEGER, lat float, lng float)
---
+++ RequeteSelectionDataOnArea.sql
@@ -0,0 +1,1 @@
+﻿select * from velovdata where station_name = 11 and  ( extract(HOUR FROM last_update_fme) = 20 or extract(HOUR FROM last_update_fme) = 21 )
