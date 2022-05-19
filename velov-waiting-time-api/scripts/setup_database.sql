750d4b211222bfd4bd7a4b64708b74e058c76b77
Pierre RICHAUD <pierre.richaud@smile.fr>
2022-05-19 15:30:49 +0200

    velov-waiting-time-api/scripts/setup_database.sql

--- velov-waiting-time-api/scripts/setup_database.sql
+++ velov-waiting-time-api/scripts/setup_database.sql
@@ -6,8 +6,6 @@ CREATE USER dev PASSWORD 'velov';
 
 CREATE TABLE stations_data ( station_id integer, available_bikes integer, last_update timestamp without time zone, last_update_fme timestamp without time zone, status character varying(20), available_bike_stands integer, availabilitycode integer );
 
-CREATE TABLE stations (station_id integer, name character varying(50), number integer, lat integer, lng integer, bike_stands integer, address character varying(50), code_insee integer, commune character varying(20))
+CREATE TABLE stations (station_id integer, name character varying(50), number integer, lat integer, lng integer, bike_stands integer, address character varying(50), code_insee integer, commune character varying(20) PRIMARY KEY (station_id) );
 
-CREATE TABLE stations_hours (station_id integer, hour integer, exponentiation numeric, shape numeric, loc numeric, scale numeric)
-
-
+CREATE TABLE stations_hours (station_id integer, hour integer, exponentiation numeric, shape numeric, loc numeric, scale numeric PRIMARY KEY (station_id, hour) );
