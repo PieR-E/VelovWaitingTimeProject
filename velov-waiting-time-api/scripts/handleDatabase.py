68f496e3c0952ddd3b7eaab4c0d6d5116c4d4828
Pierre RICHAUD <pierre.richaud@smile.fr>
2022-05-25 16:48:32 +0200

    fix(scripts): fix python scripts

--- velov-waiting-time-api/scripts/handleDatabase.py
+++ velov-waiting-time-api/scripts/handleDatabase.py
@@ -16,7 +16,7 @@ conn = psycopg2.connect(database = "velov",
 sqlDropStationsHours = "DROP TABLE stations_hours"
 sqlCreateStationsHours = "CREATE TABLE stations_hours (station_id integer, hour integer, exponentiation numeric, shape numeric, loc numeric, scale numeric, PRIMARY KEY(station_id, hour))"
 sqlDropStations = "DROP TABLE stations"
-sqlCreateStations = "CREATE TABLE stations (station_id integer PRIMARY KEY, name character varying(50), number integer, lat integer, lng integer, bike_stands integer, address character varying(50), code_insee integer, commune character varying(20))"
+sqlCreateStations = "CREATE TABLE stations (station_id integer PRIMARY KEY, name character varying(50), number integer, lat numeric, lng numeric, bike_stands integer, address character varying(50), code_insee integer, commune character varying(20))"
 sqlDropStationsData = "DROP TABLE stations_data"
 sqlCreateStationsData = "CREATE TABLE stations_data ( station_id integer, available_bikes integer, last_update timestamp without time zone, last_update_fme timestamp without time zone, status character varying(20), available_bike_stands integer, availabilitycode integer )"
 
